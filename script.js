54c8258f7f0cc43cfc9245831e0d8a6f9a96ea44
Brandy McArthur <bdm.mcarthur@gmail.com>
2018-11-21 12:21:09 -0500

    Added placeholder sounds

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-var keys = {
-a: 65,
-s: 83,
-d: 68,
-f: 70,
-g: 71,
-h: 72,
-j: 74,
-k: 75,
-l: 76,
-}
 
+var keys = {a: {
+    num: 65,
+    urls: 'bubbles',
+},
+s: {
+    num: 83,
+    urls: 'clay'
+},
+d: {
+    num: 68,
+    urls: 'confetti',
+},
+f: {
+    num: 70,
+    urls: 'corona',
+},
+g: {
+    num: 71,
+    urls: 'dotted-spiral',
+},
+h: {
+    num: 72,
+    urls: 'flash-1',
+},
+j: {
+    num: 74,
+    urls: 'flash-2',
+},
+k: {
+    num: 75,
+    urls: 'flash-3',
+},
+l: {
+    num: 76,
+        urls: 'glimmer',
+}
+}
 
 window.addEventListener("keydown", keyboardPress);
 window.addEventListener("keyup", keyboardPress);
@@ -17,11 +43,12 @@ window.addEventListener("keyup", keyboardPress);
 function keyboardPress(event){
     //letter
     var keyPress = event.key
-    console.log(keyPress)
+
     //number
-    var final = keys[keyPress];
-    console.log(final)
+    var final = keys[keyPress].num;
+
     var entry = document.getElementById(keyPress)
     entry.classList.toggle("clicked");
-
+    //new Audio('sounds/' + keyPress + '.mp3').play();
+    new Audio('sounds/' + keys[keyPress].urls + '.mp3').play();
 }
