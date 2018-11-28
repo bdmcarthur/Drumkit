79b80c2ab6768ae0f8f6023c339ad820a2fae94a
Brandy McArthur <bdm.mcarthur@gmail.com>
2018-11-27 21:42:31 -0500

    Changed the sounds

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 
 var keys = {a: {
     num: 65,
-    urls: 'bubbles',
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/kraftwerkesque/12[kb]kik2.aif.mp3',
 },
 s: {
     num: 83,
-    urls: 'clay'
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/punches%20hits%20discoblasts%20bombs/61[kb]stereo_punch.aif.mp3'
 },
 d: {
     num: 68,
-    urls: 'confetti',
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/kraftwerkesque/8[kb]zap5.aif.mp3',
 },
 f: {
     num: 70,
-    urls: 'corona',
+    urls: 'https://sampleswap.org/samples-ghost/VOCALS%20and%20SPOKEN%20WORD/percussive%20or%20punchy/48[kb]voice_uh-clap.aif.mp3',
 },
 g: {
     num: 71,
-    urls: 'dotted-spiral',
+    urls: 'https://sampleswap.org/samples-ghost/INSTRUMENTS%20single%20samples/synth/40[kb]kewl_reverse.aif.mp3',
 },
 h: {
     num: 72,
-    urls: 'flash-1',
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/various%20weird%20hits/61[kb]catcall.aif.mp3',
 },
 j: {
     num: 74,
-    urls: 'flash-2',
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/punches%20hits%20discoblasts%20bombs/1181[kb]drop-kick-07.wav.mp3',
 },
 k: {
     num: 75,
-    urls: 'flash-3',
+    urls: 'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/EC-guns-hats-kicks-snares/EC-guns/38[kb]ec-gunz13.wav.mp3',
 },
 l: {
     num: 76,
-        urls: 'glimmer',
+        urls: 'https://sampleswap.org/samples-ghost/DRUMS%20and%20SINGLE%20HITS/various%20weird%20hits/102[kb]coins-thrown.wav.mp3',
 }
 }
 
@@ -47,7 +47,7 @@ function keyboardPress(event){
 
     //adds class
     img.classList.add("clicked");
-    new Audio('sounds/' + keys[letter].urls + '.mp3').play();
+    new Audio(keys[letter].urls).play();
 
     //adds event listener to each image
     for(var i = 0; i < 9; i++){
